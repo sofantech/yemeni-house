05dae17bd89c5b583746ce85f68f8975b81056bf
sofan-tech <2354sofantech.net@gmail.com>
2021-12-26 21:54:01 +0300

    upload design

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn =>{
+    btn.onclick = () =>{
+        let src = btn.getAttribute('data-src');
+        document.querySelector('.about .video-container .video').src = src;
+    }
+})
+let my=["Ahmed","Mazero","Elham","Osama","Gamal","Ameer"];
+console.log(my.reverse().slice(2));
+console.log(my.slice(3,5));
